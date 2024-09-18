8ae2990168cdd65b6aeb00525d0bef0ce14a2089
Gormery Kombo Wanjiru <gormerykw@uia.no>
2024-09-18 11:25:53 +0200

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -16,13 +16,16 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
+		appDir: 'app', // Required as the default is _app
+
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: deployTarget === 'auto' ? adapterAuto() : deployTarget === 'static' ? adapterStatic() : adapterNode(),
 		paths: {
-            base: environment === 'production' ? '/sveltekit-github-pages' : '',
-        }
+            base: dev ? '' : process.env.BASE_PATH,
+        },
+
 	}
 };
 
